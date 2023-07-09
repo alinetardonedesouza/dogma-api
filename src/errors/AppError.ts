146f7c0b3959d80afc134cd61b376945df959914
AlineTardoneHagens <aline.tardone@hagens.com.br>
2023-07-09 16:13:59 -0300

    🎉 First commit: Boilerplate

---
+++ src/errors/AppError.ts
@@ -0,0 +1,10 @@
+export class AppError {
+
+    public readonly message: string;
+    public readonly statusCode: number;
+
+    constructor(message: string, status = 400) {
+        this.message = message;
+        this.statusCode = status
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import "express-async-errors"
+import express, { NextFunction, Request, Response } from 'express';
+import { routes } from './routes';
+import { AppError } from "./errors/AppError";
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            status: "error",
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`,
+    })
+})
+
+app.listen(port, () => console.log('Server listening on port: ' + port));
