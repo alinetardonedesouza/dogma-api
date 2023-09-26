7c961ee8f68e6d576e5ae86cae0f3d74adb47134
AlineTardoneHagens <aline.tardone@hagens.com.br>
2023-09-25 21:51:21 -0300

    🚧 📝 Documentação em Swagger em construção

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { userRoutes } from "../../modules/users/routes/user.routes";
 import { petsRoutes } from "../../modules/pets/routes/pets.routes";
 import { stressRoutes } from "../../modules/stress/routes/stress.routes";
@@ -7,6 +7,9 @@ import { gpsRoutes } from "../../modules/gps/routes/gps.routes";
 import { soundRoutes } from "../../modules/sound/routes/sound.routes";
 import { acelerometerRoutes } from "../../modules/acelerometer/routes/acelerometer.routes";
 import { heartRateRoutes } from "../../modules/heartRate/routes/heartRate.routes";
+import swaggerDocument from "../../docs/swagger.json"
+
+import swaggerUi from 'swagger-ui-express';
 
 const routes = Router();
 
@@ -18,5 +21,16 @@ routes.use("/gps", gpsRoutes)
 routes.use("/sound", soundRoutes)
 routes.use("/acelerometer", acelerometerRoutes)
 routes.use("/heart-rate", heartRateRoutes)
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+routes.get('/', (_req: Request, res: Response) => {
+  return res.status(301).redirect('/api-docs');
+});
+
+routes.all('*', (request: Request, response: Response) => {
+  response.status(404).send(`<h1>This route ${request.url} does not exist</h1>`);
+});
 
 export { routes };
