2743fcbf21fa1a90691b03551cdea5627d35a2bc
AlineTardoneHagens <aline.tardone@hagens.com.br>
2023-11-13 19:37:23 -0300

    🔥 removidos modulos de estresse e batimentos cardiacos

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,12 +1,10 @@
 import { Request, Response, Router } from "express";
 import { userRoutes } from "../../modules/users/routes/user.routes";
 import { petsRoutes } from "../../modules/pets/routes/pets.routes";
-import { stressRoutes } from "../../modules/stress/routes/stress.routes";
 import { collarRoutes } from "../../modules/collar/routes/collar.routes"
 import { gpsRoutes } from "../../modules/gps/routes/gps.routes";
 import { soundRoutes } from "../../modules/sound/routes/sound.routes";
 import { acelerometerRoutes } from "../../modules/acelerometer/routes/acelerometer.routes";
-import { heartRateRoutes } from "../../modules/heartRate/routes/heartRate.routes";
 import swaggerDocument from "../../docs/swagger.json"
 
 import swaggerUi from 'swagger-ui-express';
@@ -15,12 +13,10 @@ const routes = Router();
 
 routes.use("/users", userRoutes)
 routes.use("/pets", petsRoutes)
-// routes.use("/stress", stressRoutes)
 routes.use("/collar", collarRoutes)
 routes.use("/gps", gpsRoutes)
 routes.use("/sound", soundRoutes)
 routes.use("/acelerometer", acelerometerRoutes)
-// routes.use("/heart-rate", heartRateRoutes)
 
 routes.use('/api-docs', swaggerUi.serve);
 routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
